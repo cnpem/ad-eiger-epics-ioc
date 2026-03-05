1323ad3e43c9f53e8bb954850bea8a7e94fe1c00
Gustavo de Souza dos Reis <gustavo.reis@lnls.br>
2026-03-05 14:48:11 -0300

    docker: use standardized containerization scheme.
    `RELEASE` file has been kept with the specification of the required
    EPICS dependencies. This should help to build the IOC outside our
    standardized container scheme.
    
    `st.cmd` has been written in way so support a local initialization
    script used at Sirius beamlines. This includes the commented line
    specifying the container image, the mounting volume and the log
    enabling. Mounting /tmp for testing purposes and /ibira, the Sirius
    main storage.

--- iocBoot/iocPilatus4/st.cmd
+++ iocBoot/iocPilatus4/st.cmd
@@ -1,4 +1,10 @@
-#!../../bin/linux-x86_64/Pilatus4
+#!/opt/ad-pilatus4-epics-ioc/bin/linux-x86_64/Pilatus4
+# -*- container-image: ghcr.io/cnpem/ad-pilatus4-epics-ioc
+# -*- mount: /tmp:/tmp
+# -*- mount: /ibira:/ibira
+# -*- log!
+
+cd /opt/ad-pilatus4-epics-ioc/iocBoot/iocPilatus4
 
 < envPaths
 
